368b1f6b3eaa25bd824e9f11b7dc27509439d198
Awais Isane <awaisisane@gmail.com>
2021-06-30 23:30:26 +0530

    refactored

---
+++ part4/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+
+const mongoUrl = config.MONGODB_URI
+logger.info("connecting to mongodb ",mongoUrl)
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info("connected to mongo db")
+  }
+  )
+  .catch((error) => {
+    logger.error("error is ",error.message)
+  })
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs',blogsRouter)
+
+module.exports = app
--- part4/index.js
+++ part4/index.js
@@ -1,31 +1,7 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger')
+const app = require('./app')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
-
-
-const mongoUrl = config.MONGODB_URI
-logger.info("connecting to mongodb")
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {
-    logger.info("connected to mongo db")
-  }
-  )
-  .catch((error) => {
-    logger.error("error is ",error.message)
-  })
-
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs',blogsRouter)
-
+const logger = require('./utils/logger')
 
 const port = config.PORT
 app.listen(port, () => {
