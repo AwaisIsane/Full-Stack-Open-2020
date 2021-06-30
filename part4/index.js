7241050141d8c954d407129f4f74490a2c755d47
Awais Isane <awaisisane@gmail.com>
2021-06-30 22:13:31 +0530

    stuctured the backend

--- part4/index.js
+++ part4/index.js
@@ -1,44 +1,33 @@
-require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
+const mongoUrl = config.MONGODB_URI
+logger.info("connecting to mongodb")
 
-const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info("connected to mongo db")
+  }
+  )
+  .catch((error) => {
+    logger.error("error is ",error.message)
+  })
+
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs',blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const port = config.PORT
+app.listen(port, () => {
+  logger.info(`Server running on port ${port}`)
 })
